39817dcb556b85563f3ac59dc0ba461c64eac43c
lyl <lylactive@gmail.com>
2022-06-24 09:38:47 +0800

    feat: doc,dart_vlc

---
+++ lib/vlc_demo.dart
@@ -0,0 +1,34 @@
+// import 'package:dart_vlc/dart_vlc.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_travel/webview_desktop.dart';
+
+// void main(List<String> args) {
+//   DartVLC.initialize();
+//   runApp(VlcApp());
+// }
+
+// class VlcApp extends StatefulWidget {
+//   VlcApp({Key? key}) : super(key: key);
+
+//   @override
+//   State<VlcApp> createState() => _VlcAppState();
+// }
+
+// class _VlcAppState extends State<VlcApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     // return GestureDetector(
+//     //   child: Text("data"),
+//     //   onTap: () {
+//     //     final player = Player(id: 69420);
+//     //     player.open(
+//     //       Media.network("https://youtu.be/g-0B_Vfc9qM"),
+//     //       autoStart: false,
+//     //     );
+//     //   },
+//     // );
+//     return Container(
+//       child: Text("data"),
+//     );
+//   }
+// }
