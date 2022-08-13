7a3d83ea312cd3d6f9d096cac34d2af7df255f45
lyl <lylactive@gmail.com>
2022-08-13 11:37:12 +0800

    feat: webview for support platform

--- lib/webview_flutter_demo/webview_flutter_main.dart
+++ lib/webview_flutter_demo/webview_flutter_main.dart
@@ -45,57 +45,61 @@ class _WebviewFlutterMainState extends State<WebviewFlutterMain> {
       home: Container(
         color: const Color.fromARGB(255, 121, 199, 235),
         child: Center(
-          child: WebView(
-            // javascriptChannels: {
-            //   channelForBridge,
-            // },
-            javascriptChannels: <JavascriptChannel>{
-              JavascriptChannel(
-                  name: "webLogin",
-                  onMessageReceived: (msg) {
-                    debugPrint(
-                        "openw [webLogin] javascriptChannels,onMessageReceived, msg=${msg.message}");
-                  }),
-              // JavascriptChannel(
-              //     name: "WebViewJavascriptBridgcallHandler",
-              //     onMessageReceived: (msg) {
-              //       debugPrint(
-              //           "openw [WebViewJavascriptBridgcallHandler] javascriptChannels,onMessageReceived, msg=$msg");
-              //     }),
-              JavascriptChannel(
-                  name: "MoonBridge",
-                  onMessageReceived: (msg) {
-                    debugPrint(
-                        "openw [MoonBridge] javascriptChannels,onMessageReceived, msg=${msg.message}");
-                  }),
-            },
-            // initialUrl: "https://daoxue.okay.cn/checkLogin.html?loginType=xdf",
-            initialUrl: "http://10.60.110.129:8080/web_page/test.html",
-            // initialUrl:
-            //     "https://daoxue.okay.cn/teaching.html#/room?cid=545373&uid=62952576952&role=1&pure=true&tab=false&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzSW4iOiI2MDQ4MDAiLCJzeXN0ZW1JZCI6IjYyOTUyNTc2OTUyIiwibG9naW5UeXBlIjoiMyIsInNoYXJlVG9rZW4iOiJmYWxzZSIsInVzZXJUeXBlIjoiMSIsIm9yZ0lkIjoiNTQiLCJ0aW1lc3RhbXAiOiIxNjU5MzIxNzkxOTY4In0.0MxVuSsLxSBNSj1u2QxDCZabplWr_GC1lrKIB61lLuY&start=none&version=1.6.3.199",
-            // initialUrl:
-            //     "http://10.60.110.167:7777/#/room?cid=545373&uid=62952576952&role=1&pure=true&tab=false&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzSW4iOiI2MDQ4MDAiLCJzeXN0ZW1JZCI6IjYyOTUyNTc2OTUyIiwibG9naW5UeXBlIjoiMyIsInNoYXJlVG9rZW4iOiJmYWxzZSIsInVzZXJUeXBlIjoiMSIsIm9yZ0lkIjoiNTQiLCJ0aW1lc3RhbXAiOiIxNjU5MzIxNzkxOTY4In0.0MxVuSsLxSBNSj1u2QxDCZabplWr_GC1lrKIB61lLuY&start=none&version=1.6.3.199",
-            // initialUrl: "http://10.60.110.167:7777/#/",
-            // initialUrl: "https://www.126.com",
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (_) {
-              print("openw onWebViewCreated......");
-              _controller.complete(_);
-            },
-            onPageStarted: (url) {
-              print("openw onPageStarted, url=$url");
-            },
-            onPageFinished: (url) {
-              print("openw onPageFinished, url=$url");
-            },
-            navigationDelegate: (NavigationRequest navigationRequest) {
-              if (navigationRequest.url
-                  .startsWith("WebViewJavascriptBridg.callHandler")) {
-                print('openw blocking navigation to $navigationRequest}');
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
+          child: Visibility(
+            visible: Platform.isAndroid || Platform.isIOS,
+            replacement: const Text("current webview only support android&ios"),
+            child: WebView(
+              // javascriptChannels: {
+              //   channelForBridge,
+              // },
+              javascriptChannels: <JavascriptChannel>{
+                JavascriptChannel(
+                    name: "webLogin",
+                    onMessageReceived: (msg) {
+                      debugPrint(
+                          "openw [webLogin] javascriptChannels,onMessageReceived, msg=${msg.message}");
+                    }),
+                // JavascriptChannel(
+                //     name: "WebViewJavascriptBridgcallHandler",
+                //     onMessageReceived: (msg) {
+                //       debugPrint(
+                //           "openw [WebViewJavascriptBridgcallHandler] javascriptChannels,onMessageReceived, msg=$msg");
+                //     }),
+                JavascriptChannel(
+                    name: "MoonBridge",
+                    onMessageReceived: (msg) {
+                      debugPrint(
+                          "openw [MoonBridge] javascriptChannels,onMessageReceived, msg=${msg.message}");
+                    }),
+              },
+              // initialUrl: "https://daoxue.okay.cn/checkLogin.html?loginType=xdf",
+              initialUrl: "http://10.60.110.129:8080/web_page/test.html",
+              // initialUrl:
+              //     "https://daoxue.okay.cn/teaching.html#/room?cid=545373&uid=62952576952&role=1&pure=true&tab=false&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzSW4iOiI2MDQ4MDAiLCJzeXN0ZW1JZCI6IjYyOTUyNTc2OTUyIiwibG9naW5UeXBlIjoiMyIsInNoYXJlVG9rZW4iOiJmYWxzZSIsInVzZXJUeXBlIjoiMSIsIm9yZ0lkIjoiNTQiLCJ0aW1lc3RhbXAiOiIxNjU5MzIxNzkxOTY4In0.0MxVuSsLxSBNSj1u2QxDCZabplWr_GC1lrKIB61lLuY&start=none&version=1.6.3.199",
+              // initialUrl:
+              //     "http://10.60.110.167:7777/#/room?cid=545373&uid=62952576952&role=1&pure=true&tab=false&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHBpcmVzSW4iOiI2MDQ4MDAiLCJzeXN0ZW1JZCI6IjYyOTUyNTc2OTUyIiwibG9naW5UeXBlIjoiMyIsInNoYXJlVG9rZW4iOiJmYWxzZSIsInVzZXJUeXBlIjoiMSIsIm9yZ0lkIjoiNTQiLCJ0aW1lc3RhbXAiOiIxNjU5MzIxNzkxOTY4In0.0MxVuSsLxSBNSj1u2QxDCZabplWr_GC1lrKIB61lLuY&start=none&version=1.6.3.199",
+              // initialUrl: "http://10.60.110.167:7777/#/",
+              // initialUrl: "https://www.126.com",
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (_) {
+                print("openw onWebViewCreated......");
+                _controller.complete(_);
+              },
+              onPageStarted: (url) {
+                print("openw onPageStarted, url=$url");
+              },
+              onPageFinished: (url) {
+                print("openw onPageFinished, url=$url");
+              },
+              navigationDelegate: (NavigationRequest navigationRequest) {
+                if (navigationRequest.url
+                    .startsWith("WebViewJavascriptBridg.callHandler")) {
+                  print('openw blocking navigation to $navigationRequest}');
+                  return NavigationDecision.prevent;
+                }
+                return NavigationDecision.navigate;
+              },
+            ),
           ),
         ),
       ),
